0140bff0b75425e23f28e626efdb18818c999c4a
JosedLeonett <josed.95@gmail.com>
2026-05-23 17:49:12 -0300

    feat(w03): dynamic header and footer via fetch partials

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
-import { updateCartCount } from './utils.mjs';
+import { updateCartCount, loadHeaderFooter } from './utils.mjs';
 
-updateCartCount();
+loadHeaderFooter(updateCartCount);
 
 const listElement = document.querySelector('.product-list');
 if (listElement) {
